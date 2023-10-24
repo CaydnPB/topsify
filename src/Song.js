3fe707bd6e5d441baf0ed7915c144e7b53972475
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-10-24 12:01:44 +0100

    Add static property and method

--- src/Song.js
+++ src/Song.js
@@ -1,21 +1,28 @@
 class Song {
+    static catalogue = []
+    static addSong (title) {
+        Song.catalogue.push (title)
+    }
+
     constructor(title, duration) {
         this.title = title;
         this.duration = duration;
+        Song.addSong (title)
+    }
+
+    logDetails() {
+        console.log(`The song ${this.title} is ${this.duration} seconds long.`)
     }
 
     displayDuration() {
         let seconds = this.duration;
         let minutes = 0;
-
         while (seconds > 60) {
             seconds -= 60;
             minutes++;
         }
-
         minutes = (minutes < 10 ? "0" : "") + minutes;
         seconds = (seconds < 10 ? "0" : "") + seconds;
-
         return minutes + ":" + seconds;
     }
 }
